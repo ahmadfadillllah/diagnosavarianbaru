e6a719c29daefe931113883a43b362495d539cea
ahmadfadillllah <ahmadfadillah502@gmail.com>
2022-04-12 14:01:28 +0800

    Update avatar

--- resources/views/dashboard/master/header.blade.php
+++ resources/views/dashboard/master/header.blade.php
@@ -26,13 +26,17 @@
             <li class="nav-item nav-search"><a class="nav-link nav-link-search"><i class="ficon" data-feather="search"></i></a>
                 <div class="search-input">
                     <div class="search-input-icon"><i data-feather="search"></i></div>
-                    <input class="form-control input" type="text" placeholder="Explore Vuexy..." tabindex="-1" data-search="search">
+                    <input class="form-control input" type="text" placeholder="Explore..." tabindex="-1" data-search="search">
                     <div class="search-input-close"><i data-feather="x"></i></div>
                     <ul class="search-list search-list-main"></ul>
                 </div>
             </li>
             <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">{{ Auth::user()->name }}</span><span class="user-status">{{ Auth::user()->role }}</span></div><span class="avatar"><img class="round" src="{{ asset('vuexy') }}/app-assets/images/portrait/small/avatar-s-11.jpg" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
+                @if (Auth::user()->role == 'admin')
+                <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">{{ Auth::user()->name }}</span><span class="user-status">{{ Auth::user()->role }}</span></div><span class="avatar"><img class="round" src="{{ asset('vuexy') }}/app-assets/images/portrait/small/avatar-s-11.jpg" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
+                @elseif (Auth::user()->role == 'pakar')
+                <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">{{ Auth::user()->name }}</span><span class="user-status">{{ Auth::user()->role }}</span></div><span class="avatar"><img class="round" src="{{ asset('vuexy') }}/app-assets/images/portrait/small/avatar.jpg" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
+                @endif
                 </a>
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
                     <a class="dropdown-item" href="{{ route('profil.index') }}"><i class="me-50" data-feather="user"></i> Profile</a>
